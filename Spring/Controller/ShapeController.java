fac5987efd5a906b23712e043a2cda8904fa5d32
Aly-El-Din <124249146+Aly-El-Din@users.noreply.github.com>
2023-12-06 00:33:32 +0200

    Add files via upload

--- Spring/Controller/ShapeController.java
+++ Spring/Controller/ShapeController.java
@@ -1,9 +1,13 @@
 package dev.PainterApplication.Painter.Controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import dev.PainterApplication.Painter.Model.*;
 import dev.PainterApplication.Painter.Service.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @RestController
@@ -19,6 +23,9 @@ public class ShapeController {
     }
     @PostMapping("/create")
     public Shape addShape(@RequestBody ShapeRequest shapeRequest){
+        return createShape(shapeRequest);
+    }
+    private Shape createShape(ShapeRequest shapeRequest){
         Shape shape = shapeFactory.getShape(shapeRequest);
         return shapeService.addShape(shape);
     }
@@ -30,9 +37,33 @@ public class ShapeController {
     public Shape copyShape(@PathVariable String id){
         return shapeService.getClone(id);
     }
-    @GetMapping("/move")
-    public Shape moveShape(@RequestParam("id") String id,@RequestParam("x")double x,@RequestParam("y")double y){
-        return shapeService.moveShape(id,x,y);
+    @PostMapping("/modify")
+    public void modifyShape(@RequestBody ShapeRequest shapeRequest){
+        Shape modifiedShape=createShape(shapeRequest);
+        shapeService.modifyShape(modifiedShape);
     }
+    @GetMapping("/save")
+    public String Save(@RequestParam String filePath) throws IOException {
+        System.out.println(filePath);
+        shapeService.ShapesSave(filePath);
+        return filePath;
+    }
+    @GetMapping("/load")
+    public ArrayList<Shape> load(@RequestParam String filePath)throws IOException{
+        ArrayList<Shape>JSONShapes=readXmlFile(filePath);
 
+        String jsonShapes=convertToJson(JSONShapes);
+
+        System.out.println(JSONShapes);
+
+        return JSONShapes;
+    }
+    private ArrayList<Shape>readXmlFile(String xmlFilePath)throws IOException{
+        XmlMapper xmlMapper=new XmlMapper();
+        return xmlMapper.readValue(new File(xmlFilePath), ArrayList.class);
+    }
+    private String convertToJson(ArrayList<Shape>jsonShapes)throws IOException{
+        ObjectMapper objectMapper=new ObjectMapper();
+        return objectMapper.writeValueAsString(jsonShapes);
+    }
 }
