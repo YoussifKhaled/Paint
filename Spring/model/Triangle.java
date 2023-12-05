620066e3401a62adc73a12c5638d68681e4cdf6c
mohamed mahfouz <mohamadmahfouz39@gmail.com>
2023-12-05 23:43:09 +0200

    Update Triangle.java

--- Spring/model/Triangle.java
+++ Spring/model/Triangle.java
@@ -1,44 +1,30 @@
 package com.example.paint.model;
 
 public class Triangle extends Shape {
-    private double length;
-    private double[] points;
+    private double radius;
 
     public Triangle(){
 
     }
 
-    public Triangle(double length){
+    public Triangle(double radius){
 
-        this.length = length;
+        this.radius = radius;
     }
 
     public Triangle(ShapeRequest shapeRequest) {
         super(shapeRequest);
-        points = new double[6];
-        this.length = 200;
+        this.radius = 100;
+
     }
 
-    public double getLength() {
-        return length;
+    public double getRadius() {
+        return radius;
     }
 
-    public double[] getPoints() {
-        points[0] = this.x - 0.5 * this.length;
-        points[1] = this.y + 0.2887 * this.length;
-        points[2] = this.x + 0.5 * this.length;
-        points[3] = points[1];
-        points[4] = this.x;
-        points[5] = this.y - 0.5773 * this.length;
-        return points;
-    }
 
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public void setPoints(double[] points) {
-        this.points = points;
+    public void setRadius(double radius) {
+        this.radius = radius;
     }
 
     public void draw(){
