102fe07a87237e693afa03a97b0d3c881744eca0
esmailMahmouds <120949930+esmailMahmouds@users.noreply.github.com>
2023-12-06 10:40:19 +0200

    Add files via upload

--- Spring/Controller/ShapeController.java
+++ Spring/Controller/ShapeController.java
@@ -1,59 +1,69 @@
-package com.example.paint.controller;
-
-
-import com.example.paint.model.Shape;
-import com.example.paint.model.ShapeRequest;
-import com.example.paint.service.DataBase;
-import com.example.paint.service.ShapeFactory;
-import com.example.paint.service.ShapeService;
-
+package dev.PainterApplication.Painter.Controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import dev.PainterApplication.Painter.Model.*;
+import dev.PainterApplication.Painter.Service.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-
 @RestController
 @RequestMapping("/paint")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8082")
 public class ShapeController {
-
     private ShapeFactory shapeFactory;
     private ShapeService shapeService;
-    private DataBase dataBase;
-
     @Autowired
-    public ShapeController(ShapeFactory shapeFactory, ShapeService shapeService,DataBase dataBase){
+    public ShapeController(ShapeFactory shapeFactory, ShapeService shapeService){
         this.shapeFactory = shapeFactory;
         this.shapeService = shapeService;
-        this.dataBase=dataBase;
     }
-
-    @PostMapping("/shapes")
+    @PostMapping("/create")
     public Shape addShape(@RequestBody ShapeRequest shapeRequest){
+        return createShape(shapeRequest);
+    }
+    private Shape createShape(ShapeRequest shapeRequest){
         Shape shape = shapeFactory.getShape(shapeRequest);
         return shapeService.addShape(shape);
     }
-
+    @GetMapping("/delete/{id}")
+    public void deleteShape(@PathVariable String id){
+        shapeService.deleteShape(id);
+    }
+    @GetMapping("/copy/{id}")
+    public Shape copyShape(@PathVariable String id){
+        return shapeService.getClone(id);
+    }
+    @PostMapping("/modify")
+    public void modifyShape(@RequestBody ShapeRequest shapeRequest){
+        Shape modifiedShape=createShape(shapeRequest);
+        shapeService.modifyShape(modifiedShape);
+    }
     @GetMapping("/save")
-    public void Save(@RequestParam String filePath) throws IOException {
+    public String Save(@RequestParam String filePath) throws IOException {
         System.out.println(filePath);
-        String type="xml";
-        shapeService.SaveShapes(filePath,type);
+        shapeService.ShapesSave(filePath);
+        return filePath;
     }
     @GetMapping("/load")
     public ArrayList<Shape> load(@RequestParam String filePath)throws IOException{
-        String type="xml";
-        System.out.println(shapeService.loadShapes(filePath,type));
-         return  shapeService.loadShapes(filePath,type);
+        ArrayList<Shape>JSONShapes=readXmlFile(filePath);
 
+        String jsonShapes=convertToJson(JSONShapes);
+
+        System.out.println(JSONShapes);
+
+        return JSONShapes;
     }
-
-
+    private ArrayList<Shape>readXmlFile(String xmlFilePath)throws IOException{
+        XmlMapper xmlMapper=new XmlMapper();
+        return xmlMapper.readValue(new File(xmlFilePath), ArrayList.class);
+    }
+    private String convertToJson(ArrayList<Shape>jsonShapes)throws IOException{
+        ObjectMapper objectMapper=new ObjectMapper();
+        return objectMapper.writeValueAsString(jsonShapes);
+    }
 }
