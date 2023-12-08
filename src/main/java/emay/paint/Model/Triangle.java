3a8da19ccb955a8a3c086a2c41de40413992a343
Youssif Khaled <116981986+YoussifKhaled@users.noreply.github.com>
2023-12-09 00:55:46 +0200

    Final Version

---
+++ src/main/java/emay/paint/Model/Triangle.java
@@ -0,0 +1,24 @@
+package emay.paint.Model;
+
+public class Triangle extends Shape {
+    private double radius;
+    public Triangle(){}
+    public Triangle(double radius){
+        this.radius = radius;
+    }
+    public Triangle(ShapeRequest shapeRequest) {
+        super(shapeRequest);
+        if(shapeRequest.getRadius()==0){
+            this.radius = 100;
+        }
+        else{
+            this.radius=shapeRequest.getRadius();
+        }
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/test/java/emay/paint/PaintApplicationTests.java
@@ -0,0 +1,13 @@
+package emay.paint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
